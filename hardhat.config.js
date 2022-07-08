32ed5cbc6b08a492fc51d597e9af97a8ea074128
garvit <agarvit1142000@gmail.com>
2022-07-09 00:20:50 +0530

    updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,21 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+console.log(process.env.API_URL)
 module.exports = {
   solidity: "0.8.9",
-};
+  defaultNetwork: "polygon",
+  networks: {
+    hardhat: {},
+    polygon: {
+      url: process.env.API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  paths: {
+    sources: "./src/contracts",
+    tests: "./src/test",
+    cache: "./cache",
+    artifacts: "./src/artifacts"
+  }
+}
