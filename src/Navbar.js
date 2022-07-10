5b7c632e9197c24cc2a29c1bb12ef737ed96a0f4
garvit <agarvit1142000@gmail.com>
2022-07-11 00:33:04 +0530

    made navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar=()=>{
+    return (
+        <nav className='navbar'>
+        <h1>Blogg</h1>
+        <div className="links">
+           
+        </div>
+        </nav>
+
+    )
+}
+export default Navbar;
