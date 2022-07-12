b4bdac04d59ab854baaa77d08386d3936ba521ab
garvit <agarvit1142000@gmail.com>
2022-07-13 01:46:37 +0530

    Added more  functionalities for depositing and withdrawing

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,27 +1,69 @@
-import {ethers} from "ethers";
-import snakeContract from  "/home/garvit/programming/snake/src/artifacts/src/contracts/snakeContract.sol/snakeContract.json"
+import { ethers } from "ethers";
+import snakeContract from "/home/garvit/programming/snake/src/artifacts/src/contracts/snakeContract.sol/snakeContract.json";
 
-const abi = snakeContract.abi
-const contractAddress = "0xf173d147e7d0bc358a0969a6a50d503b899c1101"	
+const abi = snakeContract.abi;
+const contractAddress = "0x29fCAe3Ca2f73b38795383Ed8b3A0921BE8ea41D";
+
 async function connect() {
-    if(window.ethereum){
-      await window.ethereum.request({method : "eth_requestAccounts"})
-    }
-    else
-    {
-        console.log("no metamask");
-    }
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  } else {
+    console.log("No metamask"); // check metamask is installed or not and currency is supported or not which currency
+  }
 }
 
-async function getwalletaddress(){
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contract = new ethers.Contract(contractAddress , abi , signer)
-    return signer.getAddress();
-    console.log(signer.getChainId());
-    }
+async function getWalletAddress() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  return signer.getAddress(); // check things based on we are getting address or not  so we are connected to contract or not and if connected then do things after words make a state for that
+}
 
-export  {connect , getwalletaddress}
+async function getGlobalBalance() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  return signer.getBalance();
+}
 
+async function getVaultBalance() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  return contract.checkMyDeposit();
+}
 
+async function getVaultBalanceOf(address) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  return contract.checkOthersDeposit(address);
+}
 
+async function makeDeposit(amount) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  const txn = await contract.makeDeposit({
+    value: ethers.utils.parseEther(amount),
+  });
+  return txn.wait();
+}
+
+async function makeWithdraw(amount) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  const txn = await contract.withdrawDeposits(ethers.utils.parseEther(amount)); // withdraw more than balance in vault make try catch
+  return txn.wait();
+}
+
+export {
+  connect,
+  getWalletAddress,
+  getGlobalBalance,
+  getVaultBalance,
+  getVaultBalanceOf,
+  makeDeposit,
+  makeWithdraw,
+};
