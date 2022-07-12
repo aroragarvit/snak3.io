f8365abf3280bf94ad210dd77b9f666ffd2a6a29
garvit <agarvit1142000@gmail.com>
2022-07-13 01:47:08 +0530

    Depsit and withdraw money from vault

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,136 @@
-import {connect , getwalletaddress} from "./scripts/services"
-const Home = ()=>{
-    return (
-        <div classname="home">
-            <button onClick={()=> connect()}>Connect</button>
-            <button onClick={()=> getwalletaddress()}>getaddress</button>
+import {
+  connect,
+  getWalletAddress,
+  getGlobalBalance,
+  getVaultBalance,
+  getVaultBalanceOf,
+  makeDeposit,
+  makeWithdraw,
+} from "./scripts/services";
+import { useState, useEffect } from "react";
 
-        </div>
-    )
-}
-export default Home
+const Home = () => {
+  const [walletaddress, setwalletaddress] = useState("");
+  const [status, setstatus] = useState("connect");
+  const [globalBalance, setGlobalBalance] = useState("Fetching....");
+  const [vaultBalance, setVaultBalance] = useState("Fetching....");
+  const [amount, setAmount] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
+  const [sendAmount, setSendAmount] = useState(0);
+
+  const getBalance = async () => {
+    // setGlobalBalance(await getGlobalBalance());
+    // setVaultBalance(await getVaultBalance());
+    const balance = await getVaultBalance();
+    setVaultBalance(parseInt(balance) / 10 ** 18);
+    const gBalance = await getGlobalBalance();
+    setGlobalBalance(parseInt(gBalance) / 10 ** 18);
+  };
+
+  return (
+    <div className="home">
+      <button
+        onClick={() => {
+          // IF i refresh page and even its connected it shows connect option so check it using meta mask api and  also reload automatically if is ever disconnected
+          connect()
+            .then(() => {
+              setstatus("connected");
+              getWalletAddress().then((address) => {
+                setwalletaddress(address);
+                getBalance();
+              });
+            })
+            .catch((err) => {
+              setstatus("error");
+            });
+        }}
+      >
+        {status}
+      </button>
+      <h2>user {walletaddress} is connected</h2>
+      <h2>global balance {globalBalance}</h2>
+      <h2>vault balance {vaultBalance.toString()}</h2>
+
+      <input
+        type="number"
+        placeholder="amount"
+        onChange={(e) => {
+          setAmount(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          makeDeposit(amount).then(() => {
+            console.log("deposit made");
+            getBalance()
+              .then(() => {
+                console.log("balance updated");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        }}
+      >
+        Deposit
+      </button>
+
+      <input
+        type="text"
+        placeholder="amount"
+        onChange={(e) => {
+          setWithdrawAmount(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          makeWithdraw(withdrawAmount).then(() => {
+            console.log("deposit made");
+            getBalance()
+              .then(() => {
+                console.log("balance updated");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        }}
+      >
+        withdrawAmount
+      </button>
+
+      <input
+        type="text"
+        placeholder="amount"
+        onChange={(e) => {
+          setWithdrawAmount(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        placeholder="amount"
+        onChange={(e) => {
+          setSendAmount(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          makeWithdraw(withdrawAmount).then(() => {
+            console.log("deposit made");
+            getBalance()
+              .then(() => {
+                console.log("balance updated");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          });
+        }}
+      >
+        withdrawAmount
+      </button>
+    </div>
+  );
+};
+
+export default Home;
