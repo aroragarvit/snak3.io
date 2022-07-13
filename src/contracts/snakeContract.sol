e6e036bdea11e3c12e42979755abcf338ad34cc2
garvit <agarvit1142000@gmail.com>
2022-07-13 12:44:26 +0530

    Formated contract

--- src/contracts/snakeContract.sol
+++ src/contracts/snakeContract.sol
@@ -2,18 +2,39 @@
 pragma solidity ^0.8.7;
 
 contract snakeContract {
-    mapping(address => uint256) public deposits;
+    
+    mapping(address => uint256) public vault;
+
+    event moneyDeposited(address indexed sender, uint256 amount);
+    event moneyWithdrawn(address indexed receiver, uint256 amount);
+    event moneyTransferred(address indexed sender, address indexed receiver, uint256 amount);
 
     function makeDeposit() external payable {
-        deposits[msg.sender] = msg.value;
+        vault[msg.sender] += msg.value;         
+        emit moneyDeposited(msg.sender, msg.value);
     }
 
-    function withdrawDeposits(uint256 amount) external {
-        require(deposits[msg.sender] >= amount, "Not enough tokens");
+    function checkOthersDeposit(address checker) external view returns (uint256) {
+        return vault[checker];
+    }
+
+    function checkMyDeposit() external view returns (uint256) {
+        return vault[msg.sender];
+    }
+    
+    function sendMoney(address receiver, uint256 amount) external payable{
+        require(vault[msg.sender] >= amount);
+        vault[msg.sender] -= amount;
+        vault[receiver] += amount;
+        emit moneyTransferred(msg.sender, receiver, amount);
+    }
+
+    function withdrawDeposits(uint256 amount) external payable {
+        require(vault[msg.sender] >= amount, "Not enough tokens");
         payable(msg.sender).transfer(amount);
+        vault[msg.sender] -= amount;
+        emit moneyWithdrawn(msg.sender, amount);
     }
 
-    function seeDeposit(address checker) external view returns (uint256) {
-        return deposits[checker];
-    }
+    
 }
