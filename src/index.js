83214da5043595279c7a07a220aa4cd40bbe31e0
garvit <agarvit1142000@gmail.com>
2022-07-26 03:40:18 +0530

    Added routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./global.css";
 import App from "./App";
+import Expenses from "./routes/Expenses";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
